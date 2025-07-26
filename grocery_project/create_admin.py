5c00844ff46ce4830d100277165fbd4a0a679fa9
Nashra Mukhtar@ <nashramukhtar80@gmail.com>
2025-07-27 00:33:38 +0500

    dhjkdfhcndjk

--- grocery_project/create_admin.py
+++ grocery_project/create_admin.py
@@ -1,15 +1,11 @@
-from django.contrib.auth.models import User
-import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
 
-username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-
-try:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
-except Exception as e:
-    print(f"Error creating superuser: {e}")
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(username='admin', email='admin@example.com', password='admin123')
+            self.stdout.write(self.style.SUCCESS('Superuser created'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
